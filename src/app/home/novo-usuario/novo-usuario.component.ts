033254917db2802c55d3bc431c2159724ad945e1
andressa-raraujo <andressa.raraujo@outlook.com>
2021-10-22 21:55:11 -0300

    Criação do formulário de cadastro

---
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -0,0 +1,41 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
+
+@Component({
+  selector: 'app-novo-usuario',
+  templateUrl: './novo-usuario.component.html',
+  styleUrls: ['./novo-usuario.component.css'],
+})
+export class NovoUsuarioComponent implements OnInit {
+  novoUsuarioForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]],
+      userName: [''],
+      password: [''],
+    });
+  }
+
+  cadastrar() {
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    this.novoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe(
+      () => {
+        this.router.navigate(['']);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
